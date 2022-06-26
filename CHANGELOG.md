ff411d0deebd069a4e19628701dc6f0ea4dcd390
Baskoro Nugroho <bas.nugroho@yahoo.com>
2022-06-26 23:16:57 +0700

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for tiangkita-console
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ app/Main.hs
@@ -0,0 +1,36 @@
+import Helper (MaybeT, liftMaybeT, maybeReadInt, prompt, runMaybeT)
+
+runProgram :: IO ()
+runProgram = do
+    putStrLn "\n\n\n=============== TiangKita Validator Console ==============="
+    putStrLn $ replicate 57 '='
+    -- putStrLn $ showItem items
+    putStrLn "(a) Login  (b) Get Telkom Area  (c) Validate Eksisting Tiang  (d) Found New Tiang  (e) Log Out"
+    choice <- prompt "Input choice: "
+    case choice of
+        "a" -> do
+            putStrLn $ "Enter Token"
+            empty <- prompt "Press enter to go back"
+            runProgram
+        "b" -> do
+            putStrLn $ "Enter your location (longitude, latitude)"
+            empty <- prompt "Press enter to go back"
+            runProgram
+        "c" -> do
+            putStrLn $ "Enter your Eksisting Tiang"
+            empty <- prompt "Press enter to go back"
+            runProgram
+        "d" -> do
+            putStrLn $ "Enter your New Tiang"
+            empty <- prompt "Press enter to go back"
+            runProgram
+        "e" -> do
+            putStrLn "Exiting program..."
+            putStrLn "Goodbye!"
+        _ -> do
+            empty <- prompt "Wrong input! Press enter to try again."
+            runProgram
+
+main :: IO ()
+main = do
+    runProgram
